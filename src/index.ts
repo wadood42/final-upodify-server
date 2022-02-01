4a95849b886035f0ba35226678ce303a67feeac2
Abdul <wbehzad12@gmail.com>
2022-02-01 14:24:14 -0600

    connected to mongo

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+import express from "express";
+import "dotenv/config";
+const app = express();
+import { connect } from "mongoose";
+
+async function run(): Promise<void> {
+  if (process.env.MONGO_URI) {
+    app.listen(process.env.PORT, () => {
+      console.log(`Listening on port ${process.env.PORT}`);
+    });
+    await connect(process.env.MONGO_URI);
+    console.log("Connected MongoDB");
+  }
+}
+
+run().catch((err) => {
+  console.log("Error", err);
+});
